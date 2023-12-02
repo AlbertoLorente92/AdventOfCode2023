7326e0d9f221524de498936cc87c8192ac9a5079
AlbertoLorente <alberto.lorente92@gmail.com>
2023-12-02 15:26:15 +0100

    day 1 part 1 with Linq

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+void PrintLines(string[] lines)
+{
+    foreach (string line in lines)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+int GetCalibrationValue(string line)
+{
+    List<char> digits = line.Where(character => char.IsDigit(character)).ToList();
+    StringBuilder FirstAndLastDigit = new StringBuilder().Append(digits.First()).Append(digits.Last());
+    return int.Parse(FirstAndLastDigit.ToString());
+}
+
+int Day1(string file, bool showLines)
+{
+    string[] lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, file));
+
+    if (showLines)
+    {
+        PrintLines(lines);
+    }
+
+    return lines.Select(line => GetCalibrationValue(line)).Sum();
+}
+
+
+Console.WriteLine(Day1(@"Tests\Day1\test1.txt", true));
+Console.WriteLine(Day1(@"Tests\Day1\test2.txt", false));
